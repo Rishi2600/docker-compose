d4cddd5a5dae2c3a71f6d363620dde00055e6c97
Rishi2600 <rishiraj6177@gmail.com>
2025-03-26 23:01:11 +0530

    added an http server

--- backend/index.ts
+++ backend/index.ts
@@ -1,1 +1,9 @@
-console.log("Hello via Bun!");
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hey there from the Backend")
+})
+
+app.listen(3000)
