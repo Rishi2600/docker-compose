4984b905b57dd1b3f3d78fbdae0aeb27a41cada6
Rishi2600 <rishiraj6177@gmail.com>
2025-03-28 23:14:24 +0530

    frontend changed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,19 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
 
+  const [timer, setTimer] = useState(0)
+
   return (
     <>
-      <div>this is the frontend</div>
+      <div>
+        Hi there, this is the frontend
+      </div>
+      <div>
+        <button onClick={() => {setTimer(timer + 1)}}>Increase</button>
+        <span>{timer}</span>
+      </div>
     </>
   )
 }
